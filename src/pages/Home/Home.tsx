9e7ea11f3fba4858bcee7f376dbd311ad37b20ba
mariamdatuka <mariam.datukishvili.7@gmail.com>
2023-03-04 01:37:36 +0400

    create homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import {Wrapper} from './Styles'
+import {Wrapper,Container} from './Styles'
 
 export const Home = () => {
   return (
     <>
       <Wrapper>
-        <h1>div</h1>
+        <Container>
+          <h1>Great coffee <br/>made simple.</h1>
+          <div>Start your mornings with the world’s best coffees. Try our expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule.</div>
+          <button>Create your plan</button>
+        </Container>
       </Wrapper>
     </>
   )
 }
+
+
--- src/pages/Home/Styles.ts
+++ src/pages/Home/Styles.ts
@@ -10,3 +10,42 @@ export const Wrapper=styled.div`
    margin-bottom:20px;
    border-radius:10px;
 `
+export const Container=styled.div`
+ display:flex;
+ flex-direction:column;
+ padding-left:70px;
+ padding-top:100px;
+ align-items:flex-start;
+ gap:30px;
+
+ & h1{
+   color:#fff;
+   font-size:68px;
+ }
+
+ & div{
+   width:400px;
+   height:80px;
+   color:#FEFCF7;
+   font-family:'Barlow',serif;
+   line-height:1.5;
+ }
+
+ & button{
+   width:200px;
+   border:none;
+   height:55px;
+   border-radius:6px;
+   cursor:pointer;
+   background-color:#0E8784;
+   color:#fff;
+   font-weight:bold;
+   font-family:'Fraunces', serif;
+   font-size:16px;
+   transition:all 0.3s ease-in-out;
+ }
+
+ & button:hover{
+   background-color:#66D2CF;
+ }
+`
