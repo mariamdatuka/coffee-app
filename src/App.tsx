e5abfc128644b4bc50343a28e55aa3d4ec339cb9
mariamdatuka <mariam.datukishvili.7@gmail.com>
2023-03-06 01:23:10 +0400

    create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar/Navbar';
 import {GlobalStyles} from './GlobalStyles';
 import { Home } from './pages/Home/Home';
+import {createBrowserRouter, Route, createRoutesFromElements} from 'react-router-dom'
 
 function App() {
   
